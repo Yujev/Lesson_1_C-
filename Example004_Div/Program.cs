d8a266b1a7962a3bef998dc0f99cfc061698fe41
Yujev <ustatar@ya.ru>
2022-09-18 14:58:41 +0300

    Add Example004_Div

--- Example004_Div/Program.cs
+++ Example004_Div/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* 4-й код. Нахожу дифферинциальное двух чисел
+
+int numberA = 12;
+int numberB = 5;
+Console.WriteLine(numberA / numberB);
+*/
+//Заменяю конейнер определения целых чисел "int" на контейнер вещественных чисел "double"
+
+// 4-й код. Нахожу дифферинциальное двух чисел
+
+double numberA = 12;
+double numberB = 5;
+Console.WriteLine(numberA / numberB);
+
